09050c3eb55b0d00acaaf69dcae3384e6fee39c6
Manish Kumar Choudhary <manishkumar.choudhary@hitachivantara.com>
2020-04-01 00:07:59 +0530

    Filtering

--- web-application/src/features/profiles/ProfileContent.tsx
+++ web-application/src/features/profiles/ProfileContent.tsx
@@ -3,13 +3,14 @@ import { Tab } from "semantic-ui-react";
 import ProfilePhoto from "./ProfilePhoto";
 import ProfileDescription from "./ProfileDescription";
 import ProfileFollowings from "./ProfileFollowings";
+import ProfileActivities from "./ProfileActivities";
 
 const panes = [
   { menuItem: "About", render: () => <ProfileDescription /> },
   { menuItem: "Photos", render: () => <ProfilePhoto /> },
   {
     menuItem: "Activities",
-    render: () => <Tab.Pane>Activities content</Tab.Pane>
+    render: () => <ProfileActivities/>
   },
   {
     menuItem: "Followers",
